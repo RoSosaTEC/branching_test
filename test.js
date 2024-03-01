da43ba22e1e7a94ec78e0d9f7d7aeefc57856cfc
Rodrigo Sosa <A01027913@tec.mx>
2024-03-01 15:15:03 -0600

    modifacion de archivo .js

--- test.js
+++ test.js
@@ -2,5 +2,11 @@ function helloName(nombre){
     console.log(`Hello ${nombre}, how are you doing?` )
 }
 
+function response(){
+	console.log(`I'm doing good! Hbu?`)
+}
+
 const ex = "Facundo"
 helloName(ex)
+
+
